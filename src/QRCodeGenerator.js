b91109aa45b3d14d1b3c383f19fa7c843a088558
JB1 <28571886+jayone11@users.noreply.github.com>
2023-04-09 01:43:06 -0700

    add helmet to update the title

--- src/QRCodeGenerator.js
+++ src/QRCodeGenerator.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import QRCode from "qrcode";
+import { Helmet } from "react-helmet";
 
 function QRCodeGenerator() {
   const [url, setUrl] = useState("");
@@ -14,9 +15,36 @@ function QRCodeGenerator() {
     }
   };
 
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+    document.body.appendChild(script);
+
+    const ins = document.createElement("ins");
+    ins.className = "adsbygoogle";
+    ins.style.display = "block";
+    ins.setAttribute("data-ad-client", "ca-pub-XXXXXXXXXXXXXXXX");
+    ins.setAttribute("data-ad-slot", "1234567890");
+    ins.setAttribute("data-ad-format", "auto");
+    ins.setAttribute("data-full-width-responsive", "true");
+    document.body.appendChild(ins);
+
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  }, []);
+
   return (
     <div className="qr-code-generator-container">
+      <Helmet>
+        <title>QR Code Generator</title>
+      </Helmet>
       <h1>QR Code Generator</h1>
+      <ins className="adsbygoogle"
+           style={{ display: "block" }}
+           data-ad-client="ca-pub-XXXXXXXXXXXXXXXX"
+           data-ad-slot="1234567890"
+           data-ad-format="auto"
+           data-full-width-responsive="true"></ins>
       <input
         type="text"
         value={url}
